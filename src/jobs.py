aaf1db11a877337c86522dbdb6705e55d1b3791f
Eric Santos <eric.silvasantos44@gmail.com>
2022-10-20 18:11:12 -0300

    correção

--- src/jobs.py
+++ src/jobs.py
@@ -1,8 +1,9 @@
 from functools import lru_cache
 import csv
 
+
 @lru_cache
 def read(path):
-    with open(path, encoding = "utf-8") as file:
+    with open(path, encoding="utf-8") as file:
         jobs = csv.DictReader(file, delimiter=",", quotechar='"')
         return list(jobs)
