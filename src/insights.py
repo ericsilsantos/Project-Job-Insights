3c55903561c9d65e01ddffd613a054a1a8f761ad
Eric Santos <eric.silvasantos44@gmail.com>
2022-10-24 09:30:17 -0300

    quarto requisito

--- src/insights.py
+++ src/insights.py
@@ -55,21 +55,12 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    jobs_list = read(path)
+    salaries = set()
+    for job in jobs_list:
+        if job["max_salary"].isnumeric():
+            salaries.add(int(job["max_salary"]))
+    return max(salaries)
 
 
 def get_min_salary(path):
