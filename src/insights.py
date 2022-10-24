7ebfaf6ce2b38b6f2e54e4e5f308fcf150e5d983
Eric Santos <eric.silvasantos44@gmail.com>
2022-10-24 09:33:42 -0300

    quinto requisito

--- src/insights.py
+++ src/insights.py
@@ -64,21 +64,12 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    jobs_list = read(path)
+    salaries = set()
+    for job in jobs_list:
+        if job["min_salary"].isnumeric():
+            salaries.add(int(job["min_salary"]))
+    return min(salaries)
 
 
 def matches_salary_range(job, salary):
